b0d9f9d69ba0fbb6ea7e3b035a19f9058e3f431c
RenBergun <bergundojarren17@gmail.com>
2025-04-19 13:33:57 +0800

    changed smth

--- customer-details.vb
+++ customer-details.vb
@@ -5,6 +5,10 @@
 
     Private Sub dtpBirthday_ValueChanged(sender As Object, e As EventArgs) Handles dtpBirthday.ValueChanged
         Dim res As Date = dtpBirthday.Value
-        Label9.Text = "Selected Date: " & res.ToString("MM-dd-yyyy")
+        Label9.Text = "Birthday: " & res.ToString("MM-dd-yyyy")
+    End Sub
+
+    Private Sub txtboxCarID_TextChanged(sender As Object, e As EventArgs) Handles txtboxCarID.TextChanged
+
     End Sub
 End Class
