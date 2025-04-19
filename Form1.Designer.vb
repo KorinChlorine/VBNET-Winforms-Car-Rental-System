69990a79943d015a6ae263dd6fa91df5f6200508
RenBergun <bergundojarren17@gmail.com>
2025-04-19 13:06:54 +0800

    added some sample labels

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -25,19 +25,23 @@ Partial Class Form1
         Me.Label1 = New System.Windows.Forms.Label()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.TextBox1 = New System.Windows.Forms.TextBox()
-        Me.TextBox2 = New System.Windows.Forms.TextBox()
-        Me.TextBox3 = New System.Windows.Forms.TextBox()
+        Me.txtboxName = New System.Windows.Forms.TextBox()
+        Me.txtboxAge = New System.Windows.Forms.TextBox()
         Me.Label4 = New System.Windows.Forms.Label()
         Me.Label5 = New System.Windows.Forms.Label()
         Me.Label6 = New System.Windows.Forms.Label()
         Me.Label7 = New System.Windows.Forms.Label()
-        Me.TextBox4 = New System.Windows.Forms.TextBox()
-        Me.TextBox5 = New System.Windows.Forms.TextBox()
-        Me.TextBox6 = New System.Windows.Forms.TextBox()
-        Me.TextBox7 = New System.Windows.Forms.TextBox()
-        Me.TextBox8 = New System.Windows.Forms.TextBox()
+        Me.txtboxAddress = New System.Windows.Forms.TextBox()
+        Me.txtboxCarID = New System.Windows.Forms.TextBox()
+        Me.txtboxDays = New System.Windows.Forms.TextBox()
+        Me.txtboxRecords = New System.Windows.Forms.TextBox()
         Me.Label8 = New System.Windows.Forms.Label()
+        Me.rbMale = New System.Windows.Forms.RadioButton()
+        Me.rbFemale = New System.Windows.Forms.RadioButton()
+        Me.rbOthers = New System.Windows.Forms.RadioButton()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtRes = New System.Windows.Forms.Label()
+        Me.dtpBirthday = New System.Windows.Forms.DateTimePicker()
         Me.SuspendLayout()
         '
         'Label1
@@ -70,35 +74,27 @@ Partial Class Form1
         Me.Label3.TabIndex = 2
         Me.Label3.Text = "Birthday:"
         '
-        'TextBox1
+        'txtboxName
         '
-        Me.TextBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox1.Location = New System.Drawing.Point(220, 72)
-        Me.TextBox1.Name = "TextBox1"
-        Me.TextBox1.Size = New System.Drawing.Size(100, 29)
-        Me.TextBox1.TabIndex = 3
+        Me.txtboxName.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtboxName.Location = New System.Drawing.Point(158, 72)
+        Me.txtboxName.Name = "txtboxName"
+        Me.txtboxName.Size = New System.Drawing.Size(162, 29)
+        Me.txtboxName.TabIndex = 3
         '
-        'TextBox2
+        'txtboxAge
         '
-        Me.TextBox2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox2.Location = New System.Drawing.Point(220, 137)
-        Me.TextBox2.Name = "TextBox2"
-        Me.TextBox2.Size = New System.Drawing.Size(100, 29)
-        Me.TextBox2.TabIndex = 4
-        '
-        'TextBox3
-        '
-        Me.TextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox3.Location = New System.Drawing.Point(220, 208)
-        Me.TextBox3.Name = "TextBox3"
-        Me.TextBox3.Size = New System.Drawing.Size(100, 29)
-        Me.TextBox3.TabIndex = 5
+        Me.txtboxAge.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtboxAge.Location = New System.Drawing.Point(158, 140)
+        Me.txtboxAge.Name = "txtboxAge"
+        Me.txtboxAge.Size = New System.Drawing.Size(162, 29)
+        Me.txtboxAge.TabIndex = 4
         '
         'Label4
         '
         Me.Label4.AutoSize = True
         Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label4.Location = New System.Drawing.Point(76, 284)
+        Me.Label4.Location = New System.Drawing.Point(76, 313)
         Me.Label4.Name = "Label4"
         Me.Label4.Size = New System.Drawing.Size(48, 24)
         Me.Label4.TabIndex = 6
@@ -118,7 +114,7 @@ Partial Class Form1
         '
         Me.Label6.AutoSize = True
         Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label6.Location = New System.Drawing.Point(385, 137)
+        Me.Label6.Location = New System.Drawing.Point(385, 143)
         Me.Label6.Name = "Label6"
         Me.Label6.Size = New System.Drawing.Size(196, 24)
         Me.Label6.TabIndex = 8
@@ -134,74 +130,134 @@ Partial Class Form1
         Me.Label7.TabIndex = 9
         Me.Label7.Text = "Remaining Days for currently rented car"
         '
-        'TextBox4
+        'txtboxAddress
         '
-        Me.TextBox4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox4.Location = New System.Drawing.Point(220, 284)
-        Me.TextBox4.Name = "TextBox4"
-        Me.TextBox4.Size = New System.Drawing.Size(100, 29)
-        Me.TextBox4.TabIndex = 10
+        Me.txtboxAddress.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtboxAddress.Location = New System.Drawing.Point(755, 67)
+        Me.txtboxAddress.Name = "txtboxAddress"
+        Me.txtboxAddress.Size = New System.Drawing.Size(142, 29)
+        Me.txtboxAddress.TabIndex = 11
         '
-        'TextBox5
+        'txtboxCarID
         '
-        Me.TextBox5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox5.Location = New System.Drawing.Point(755, 72)
-        Me.TextBox5.Name = "TextBox5"
-        Me.TextBox5.Size = New System.Drawing.Size(100, 29)
-        Me.TextBox5.TabIndex = 11
+        Me.txtboxCarID.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtboxCarID.Location = New System.Drawing.Point(755, 132)
+        Me.txtboxCarID.Name = "txtboxCarID"
+        Me.txtboxCarID.Size = New System.Drawing.Size(142, 29)
+        Me.txtboxCarID.TabIndex = 12
         '
-        'TextBox6
+        'txtboxDays
         '
-        Me.TextBox6.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox6.Location = New System.Drawing.Point(755, 137)
-        Me.TextBox6.Name = "TextBox6"
-        Me.TextBox6.Size = New System.Drawing.Size(100, 29)
-        Me.TextBox6.TabIndex = 12
+        Me.txtboxDays.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtboxDays.Location = New System.Drawing.Point(755, 208)
+        Me.txtboxDays.Name = "txtboxDays"
+        Me.txtboxDays.Size = New System.Drawing.Size(142, 29)
+        Me.txtboxDays.TabIndex = 13
         '
-        'TextBox7
+        'txtboxRecords
         '
-        Me.TextBox7.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox7.Location = New System.Drawing.Point(755, 208)
-        Me.TextBox7.Name = "TextBox7"
-        Me.TextBox7.Size = New System.Drawing.Size(100, 29)
-        Me.TextBox7.TabIndex = 13
-        '
-        'TextBox8
-        '
-        Me.TextBox8.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.TextBox8.Location = New System.Drawing.Point(755, 284)
-        Me.TextBox8.Name = "TextBox8"
-        Me.TextBox8.Size = New System.Drawing.Size(100, 29)
-        Me.TextBox8.TabIndex = 14
+        Me.txtboxRecords.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtboxRecords.Location = New System.Drawing.Point(755, 313)
+        Me.txtboxRecords.Name = "txtboxRecords"
+        Me.txtboxRecords.Size = New System.Drawing.Size(142, 29)
+        Me.txtboxRecords.TabIndex = 14
         '
         'Label8
         '
         Me.Label8.AutoSize = True
         Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.Label8.Location = New System.Drawing.Point(385, 287)
+        Me.Label8.Location = New System.Drawing.Point(385, 313)
         Me.Label8.Name = "Label8"
         Me.Label8.Size = New System.Drawing.Size(241, 24)
         Me.Label8.TabIndex = 15
         Me.Label8.Text = "Good Records:(True/False)"
         '
+        'rbMale
+        '
+        Me.rbMale.AutoSize = True
+        Me.rbMale.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rbMale.Location = New System.Drawing.Point(78, 340)
+        Me.rbMale.Name = "rbMale"
+        Me.rbMale.Size = New System.Drawing.Size(61, 24)
+        Me.rbMale.TabIndex = 17
+        Me.rbMale.TabStop = True
+        Me.rbMale.Text = "Male"
+        Me.rbMale.UseVisualStyleBackColor = True
+        '
+        'rbFemale
+        '
+        Me.rbFemale.AutoSize = True
+        Me.rbFemale.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rbFemale.Location = New System.Drawing.Point(78, 376)
+        Me.rbFemale.Name = "rbFemale"
+        Me.rbFemale.Size = New System.Drawing.Size(80, 24)
+        Me.rbFemale.TabIndex = 18
+        Me.rbFemale.TabStop = True
+        Me.rbFemale.Text = "Female"
+        Me.rbFemale.UseVisualStyleBackColor = True
+        '
+        'rbOthers
+        '
+        Me.rbOthers.AutoSize = True
+        Me.rbOthers.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.rbOthers.Location = New System.Drawing.Point(78, 411)
+        Me.rbOthers.Name = "rbOthers"
+        Me.rbOthers.Size = New System.Drawing.Size(195, 24)
+        Me.rbOthers.TabIndex = 19
+        Me.rbOthers.TabStop = True
+        Me.rbOthers.Text = "Prefer not to say/Others"
+        Me.rbOthers.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.Location = New System.Drawing.Point(427, 398)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(90, 37)
+        Me.Button1.TabIndex = 20
+        Me.Button1.Text = "Result:"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtRes
+        '
+        Me.txtRes.AutoSize = True
+        Me.txtRes.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtRes.Location = New System.Drawing.Point(557, 407)
+        Me.txtRes.Name = "txtRes"
+        Me.txtRes.Size = New System.Drawing.Size(57, 20)
+        Me.txtRes.TabIndex = 21
+        Me.txtRes.Text = "Label9"
+        '
+        'dtpBirthday
+        '
+        Me.dtpBirthday.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.dtpBirthday.Location = New System.Drawing.Point(162, 203)
+        Me.dtpBirthday.Name = "dtpBirthday"
+        Me.dtpBirthday.Size = New System.Drawing.Size(158, 29)
+        Me.dtpBirthday.TabIndex = 16
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(971, 450)
+        Me.ClientSize = New System.Drawing.Size(1008, 594)
+        Me.Controls.Add(Me.txtRes)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.rbOthers)
+        Me.Controls.Add(Me.rbFemale)
+        Me.Controls.Add(Me.rbMale)
+        Me.Controls.Add(Me.dtpBirthday)
         Me.Controls.Add(Me.Label8)
-        Me.Controls.Add(Me.TextBox8)
-        Me.Controls.Add(Me.TextBox7)
-        Me.Controls.Add(Me.TextBox6)
-        Me.Controls.Add(Me.TextBox5)
-        Me.Controls.Add(Me.TextBox4)
+        Me.Controls.Add(Me.txtboxRecords)
+        Me.Controls.Add(Me.txtboxDays)
+        Me.Controls.Add(Me.txtboxCarID)
+        Me.Controls.Add(Me.txtboxAddress)
         Me.Controls.Add(Me.Label7)
         Me.Controls.Add(Me.Label6)
         Me.Controls.Add(Me.Label5)
         Me.Controls.Add(Me.Label4)
-        Me.Controls.Add(Me.TextBox3)
-        Me.Controls.Add(Me.TextBox2)
-        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.txtboxAge)
+        Me.Controls.Add(Me.txtboxName)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
@@ -215,17 +271,21 @@ Partial Class Form1
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
-    Friend WithEvents TextBox1 As TextBox
-    Friend WithEvents TextBox2 As TextBox
-    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents txtboxName As TextBox
+    Friend WithEvents txtboxAge As TextBox
     Friend WithEvents Label4 As Label
     Friend WithEvents Label5 As Label
     Friend WithEvents Label6 As Label
     Friend WithEvents Label7 As Label
-    Friend WithEvents TextBox4 As TextBox
-    Friend WithEvents TextBox5 As TextBox
-    Friend WithEvents TextBox6 As TextBox
-    Friend WithEvents TextBox7 As TextBox
-    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents txtboxAddress As TextBox
+    Friend WithEvents txtboxCarID As TextBox
+    Friend WithEvents txtboxDays As TextBox
+    Friend WithEvents txtboxRecords As TextBox
     Friend WithEvents Label8 As Label
+    Friend WithEvents rbMale As RadioButton
+    Friend WithEvents rbFemale As RadioButton
+    Friend WithEvents rbOthers As RadioButton
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtRes As Label
+    Friend WithEvents dtpBirthday As DateTimePicker
 End Class
