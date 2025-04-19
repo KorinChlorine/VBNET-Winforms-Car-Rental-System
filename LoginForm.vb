f379358c6f7b7861759507a7b4060832714ecf40
ChlorineKorin <kharlasuncion1010@gmail.com>
2025-04-19 20:16:28 +0800

    fixed for video

--- LoginForm.vb
+++ LoginForm.vb
@@ -1,23 +1,20 @@
-﻿Public Class LoginForm
-    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        Dim f As String = "I:\VB Net\Car Rental System\VBNET Car Rental System\BR86Z; Turbocharged Toyota 86 _ 4K.mp4"
-        AxWindowsMediaPlayer1.URL = f
+﻿Imports System.IO
+
+Public Class LoginForm
+    Private Sub LoginForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+
+        'play video
+        Dim videoPath As String = Path.Combine(Application.StartupPath, "video.mp4")
+
+        If Not File.Exists(videoPath) Then
+            File.WriteAllBytes(videoPath, My.Resources.video)
+        End If
+
+        AxWindowsMediaPlayer1.URL = videoPath
         AxWindowsMediaPlayer1.settings.setMode("loop", True)
         AxWindowsMediaPlayer1.Ctlcontrols.play()
-    End Sub
 
-    Private Sub AxWindowsMediaPlayer1_Enter(sender As Object, e As EventArgs) Handles AxWindowsMediaPlayer1.Enter
 
-    End Sub
-
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
-
-    End Sub
-
-    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
-
-    End Sub
-
-    Private Sub Panel3_Paint(sender As Object, e As PaintEventArgs)
     End Sub
 End Class
--- My Project/Resources.Designer.vb
+++ My Project/Resources.Designer.vb
@@ -99,5 +99,15 @@ Namespace My.Resources
                 Return CType(obj,System.Drawing.Bitmap)
             End Get
         End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Byte[].
+        '''</summary>
+        Friend ReadOnly Property video() As Byte()
+            Get
+                Dim obj As Object = ResourceManager.GetObject("video", resourceCulture)
+                Return CType(obj,Byte())
+            End Get
+        End Property
     End Module
 End Namespace
