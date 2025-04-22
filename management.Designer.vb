e0d8c83637df081d2bdf52b60350637dafd668e6
Ron Tesorero <tesoreroronbenedict@gmail.com>
2025-04-22 14:22:32 +0800

    added grp box

--- management.Designer.vb
+++ management.Designer.vb
@@ -22,7 +22,31 @@ Partial Class management
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.GroupBox1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.BackColor = System.Drawing.Color.Transparent
+        Me.GroupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Cursor = System.Windows.Forms.Cursors.Default
+        Me.GroupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.GroupBox1.Location = New System.Drawing.Point(59, 129)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(257, 562)
+        Me.GroupBox1.TabIndex = 1
+        Me.GroupBox1.TabStop = False
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(0, 0)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(257, 85)
+        Me.Button1.TabIndex = 0
+        Me.Button1.UseVisualStyleBackColor = True
         '
         'management
         '
@@ -31,10 +55,14 @@ Partial Class management
         Me.BackgroundImage = Global.VBNET_Car_Rental_System.My.Resources.Resources.Management
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.ClientSize = New System.Drawing.Size(1342, 746)
+        Me.Controls.Add(Me.GroupBox1)
         Me.DoubleBuffered = True
         Me.Name = "management"
         Me.Text = "management"
+        Me.GroupBox1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button1 As Button
 End Class
