78a23fb93d58c9ef06005152b53a6e1e4e895397
ChlorineKorin <kharlasuncion1010@gmail.com>
2025-04-24 02:35:24 +0800

    added edit, add, delete car functionality

--- management.Designer.vb
+++ management.Designer.vb
@@ -22,44 +22,22 @@ Partial Class Management
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-        Me.btnSubmit = New System.Windows.Forms.Button()
-        Me.Button1 = New System.Windows.Forms.Button()
         Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
         Me.PictureBox1 = New System.Windows.Forms.PictureBox()
-        Me.GroupBox1.SuspendLayout()
+        Me.Panel3 = New System.Windows.Forms.Panel()
+        Me.RichTextBox2 = New System.Windows.Forms.RichTextBox()
+        Me.RichTextBox3 = New System.Windows.Forms.RichTextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.RoundedButton5 = New VBNET_Car_Rental_System.RoundedButton()
+        Me.RoundedButton4 = New VBNET_Car_Rental_System.RoundedButton()
+        Me.RoundedButton3 = New VBNET_Car_Rental_System.RoundedButton()
+        Me.RoundedButton2 = New VBNET_Car_Rental_System.RoundedButton()
+        Me.RoundedButton1 = New VBNET_Car_Rental_System.RoundedButton()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'GroupBox1
-        '
-        Me.GroupBox1.BackColor = System.Drawing.Color.Transparent
-        Me.GroupBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
-        Me.GroupBox1.Controls.Add(Me.btnSubmit)
-        Me.GroupBox1.Controls.Add(Me.Button1)
-        Me.GroupBox1.Cursor = System.Windows.Forms.Cursors.Default
-        Me.GroupBox1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
-        Me.GroupBox1.Location = New System.Drawing.Point(59, 129)
-        Me.GroupBox1.Name = "GroupBox1"
-        Me.GroupBox1.Size = New System.Drawing.Size(257, 562)
-        Me.GroupBox1.TabIndex = 1
-        Me.GroupBox1.TabStop = False
-        '
-        'btnSubmit
-        '
-        Me.btnSubmit.Location = New System.Drawing.Point(6, 19)
-        Me.btnSubmit.Name = "btnSubmit"
-        Me.btnSubmit.Size = New System.Drawing.Size(257, 85)
-        Me.btnSubmit.TabIndex = 1
-        Me.btnSubmit.UseVisualStyleBackColor = True
-        '
-        'Button1
-        '
-        Me.Button1.Location = New System.Drawing.Point(-6, 272)
-        Me.Button1.Name = "Button1"
-        Me.Button1.Size = New System.Drawing.Size(257, 85)
-        Me.Button1.TabIndex = 0
-        Me.Button1.UseVisualStyleBackColor = True
         '
         'OpenFileDialog1
         '
@@ -67,11 +45,160 @@ Partial Class Management
         '
         'PictureBox1
         '
-        Me.PictureBox1.Location = New System.Drawing.Point(451, 177)
+        Me.PictureBox1.BackColor = System.Drawing.Color.White
+        Me.PictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.PictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.PictureBox1.Location = New System.Drawing.Point(363, 197)
         Me.PictureBox1.Name = "PictureBox1"
-        Me.PictureBox1.Size = New System.Drawing.Size(485, 223)
+        Me.PictureBox1.Size = New System.Drawing.Size(442, 252)
         Me.PictureBox1.TabIndex = 2
         Me.PictureBox1.TabStop = False
+        '
+        'Panel3
+        '
+        Me.Panel3.AutoScroll = True
+        Me.Panel3.BackColor = System.Drawing.Color.Transparent
+        Me.Panel3.Location = New System.Drawing.Point(49, 133)
+        Me.Panel3.Name = "Panel3"
+        Me.Panel3.Size = New System.Drawing.Size(255, 496)
+        Me.Panel3.TabIndex = 6
+        '
+        'RichTextBox2
+        '
+        Me.RichTextBox2.BackColor = System.Drawing.Color.White
+        Me.RichTextBox2.Location = New System.Drawing.Point(363, 477)
+        Me.RichTextBox2.Name = "RichTextBox2"
+        Me.RichTextBox2.Size = New System.Drawing.Size(442, 141)
+        Me.RichTextBox2.TabIndex = 8
+        Me.RichTextBox2.Text = ""
+        '
+        'RichTextBox3
+        '
+        Me.RichTextBox3.BackColor = System.Drawing.Color.White
+        Me.RichTextBox3.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RichTextBox3.Location = New System.Drawing.Point(363, 133)
+        Me.RichTextBox3.Name = "RichTextBox3"
+        Me.RichTextBox3.Size = New System.Drawing.Size(442, 36)
+        Me.RichTextBox3.TabIndex = 9
+        Me.RichTextBox3.Text = ""
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.Color.Transparent
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.Color.Black
+        Me.Label1.Location = New System.Drawing.Point(130, 111)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(84, 16)
+        Me.Label1.TabIndex = 13
+        Me.Label1.Text = "List of Cars"
+        '
+        'RoundedButton5
+        '
+        Me.RoundedButton5.BackColor = System.Drawing.Color.DarkSlateBlue
+        Me.RoundedButton5.FlatAppearance.BorderSize = 0
+        Me.RoundedButton5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.RoundedButton5.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RoundedButton5.ForeColor = System.Drawing.Color.White
+        Me.RoundedButton5.Location = New System.Drawing.Point(1113, 133)
+        Me.RoundedButton5.Name = "RoundedButton5"
+        Me.RoundedButton5.Size = New System.Drawing.Size(84, 36)
+        Me.RoundedButton5.TabIndex = 12
+        Me.RoundedButton5.Text = "Restart"
+        Me.RoundedButton5.UseVisualStyleBackColor = False
+        '
+        'RoundedButton4
+        '
+        Me.RoundedButton4.BackColor = System.Drawing.Color.Crimson
+        Me.RoundedButton4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center
+        Me.RoundedButton4.FlatAppearance.BorderSize = 0
+        Me.RoundedButton4.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.RoundedButton4.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RoundedButton4.ForeColor = System.Drawing.SystemColors.Window
+        Me.RoundedButton4.Location = New System.Drawing.Point(1073, 574)
+        Me.RoundedButton4.Name = "RoundedButton4"
+        Me.RoundedButton4.Size = New System.Drawing.Size(124, 44)
+        Me.RoundedButton4.TabIndex = 11
+        Me.RoundedButton4.Text = "Delete"
+        Me.RoundedButton4.UseVisualStyleBackColor = False
+        '
+        'RoundedButton3
+        '
+        Me.RoundedButton3.BackColor = System.Drawing.Color.MediumSeaGreen
+        Me.RoundedButton3.FlatAppearance.BorderSize = 0
+        Me.RoundedButton3.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.RoundedButton3.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RoundedButton3.ForeColor = System.Drawing.SystemColors.Window
+        Me.RoundedButton3.Location = New System.Drawing.Point(824, 574)
+        Me.RoundedButton3.Name = "RoundedButton3"
+        Me.RoundedButton3.Size = New System.Drawing.Size(243, 44)
+        Me.RoundedButton3.TabIndex = 10
+        Me.RoundedButton3.Text = "Add"
+        Me.RoundedButton3.UseVisualStyleBackColor = False
+        '
+        'RoundedButton2
+        '
+        Me.RoundedButton2.BackColor = System.Drawing.SystemColors.InactiveBorder
+        Me.RoundedButton2.FlatAppearance.BorderSize = 0
+        Me.RoundedButton2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.RoundedButton2.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RoundedButton2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.RoundedButton2.Location = New System.Drawing.Point(1040, 182)
+        Me.RoundedButton2.Name = "RoundedButton2"
+        Me.RoundedButton2.Size = New System.Drawing.Size(157, 36)
+        Me.RoundedButton2.TabIndex = 7
+        Me.RoundedButton2.UseVisualStyleBackColor = False
+        '
+        'RoundedButton1
+        '
+        Me.RoundedButton1.BackColor = System.Drawing.SystemColors.InactiveBorder
+        Me.RoundedButton1.FlatAppearance.BorderSize = 0
+        Me.RoundedButton1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.RoundedButton1.Font = New System.Drawing.Font("Microsoft Sans Serif", 14.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RoundedButton1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.RoundedButton1.Location = New System.Drawing.Point(824, 182)
+        Me.RoundedButton1.Name = "RoundedButton1"
+        Me.RoundedButton1.Size = New System.Drawing.Size(185, 36)
+        Me.RoundedButton1.TabIndex = 3
+        Me.RoundedButton1.Text = "Insert Picture"
+        Me.RoundedButton1.UseVisualStyleBackColor = False
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.BackColor = System.Drawing.Color.Transparent
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Black
+        Me.Label3.Location = New System.Drawing.Point(360, 114)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(93, 16)
+        Me.Label3.TabIndex = 15
+        Me.Label3.Text = "Name of Car"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.BackColor = System.Drawing.Color.Transparent
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.Color.Black
+        Me.Label4.Location = New System.Drawing.Point(360, 458)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(56, 16)
+        Me.Label4.TabIndex = 16
+        Me.Label4.Text = "Details"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.BackColor = System.Drawing.Color.Transparent
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.ForeColor = System.Drawing.Color.Black
+        Me.Label2.Location = New System.Drawing.Point(360, 178)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(50, 16)
+        Me.Label2.TabIndex = 17
+        Me.Label2.Text = "Image"
         '
         'Management
         '
@@ -79,20 +206,40 @@ Partial Class Management
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.BackgroundImage = Global.VBNET_Car_Rental_System.My.Resources.Resources.Management
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
-        Me.ClientSize = New System.Drawing.Size(1342, 746)
+        Me.ClientSize = New System.Drawing.Size(1264, 681)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.RoundedButton5)
+        Me.Controls.Add(Me.RoundedButton4)
+        Me.Controls.Add(Me.RoundedButton3)
+        Me.Controls.Add(Me.RichTextBox3)
+        Me.Controls.Add(Me.RichTextBox2)
+        Me.Controls.Add(Me.RoundedButton2)
+        Me.Controls.Add(Me.Panel3)
+        Me.Controls.Add(Me.RoundedButton1)
         Me.Controls.Add(Me.PictureBox1)
-        Me.Controls.Add(Me.GroupBox1)
         Me.DoubleBuffered = True
         Me.Name = "Management"
         Me.Text = "management"
-        Me.GroupBox1.ResumeLayout(False)
         CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
-    Friend WithEvents GroupBox1 As GroupBox
-    Friend WithEvents Button1 As Button
     Friend WithEvents OpenFileDialog1 As OpenFileDialog
     Friend WithEvents PictureBox1 As PictureBox
-    Friend WithEvents btnSubmit As Button
+    Friend WithEvents RoundedButton1 As RoundedButton
+    Friend WithEvents Panel3 As Panel
+    Friend WithEvents RoundedButton2 As RoundedButton
+    Friend WithEvents RichTextBox2 As RichTextBox
+    Friend WithEvents RichTextBox3 As RichTextBox
+    Friend WithEvents RoundedButton3 As RoundedButton
+    Friend WithEvents RoundedButton4 As RoundedButton
+    Friend WithEvents RoundedButton5 As RoundedButton
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label2 As Label
 End Class
--- management.vb
+++ management.vb
@@ -1,57 +1,118 @@
 ﻿Public Class Management
-    Dim selectedImage As Image
     Class Cars
         Dim Name As String
         Dim Price As Integer
         Dim Details As String
-
-
-
-
-        Function HondaCivic() As (Name As String, Price As Integer, Details As String)
-            Return ("Honda Civic", 100000, "Placeholder")
-        End Function
-
-
-
-
-
-
     End Class
 
+    Private selectedPanel As Panel = Nothing
+    Private selectedImage As Image
+
     Private Sub Management_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-
-
-
     End Sub
 
-    Private Sub OpenFileDialog1_FileOk(sender As Object, e As System.ComponentModel.CancelEventArgs) Handles OpenFileDialog1.FileOk
-
-        ' Dim openFileDialog As New OpenFileDialog()
-        'OpenFileDialog1.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif"
-        ' If openFileDialog.ShowDialog() = DialogResult.OK Then
-        'selectedImage = Image.FromFile(openFileDialog.FileName)
-        'PictureBox1.Image = SelectedImage ' Display image preview
-        ' End If
-
-        'PictureBox1.Image = Image.FromFile(OpenFileDialog1.FileName)
-    End Sub
-
-    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
         Me.DialogResult = DialogResult.OK
         Me.Close()
     End Sub
 
-    Private Sub btnSubmit_Click(sender As Object, e As EventArgs) Handles btnSubmit.Click
+    Private Sub RoundedButton1_Click(sender As Object, e As EventArgs) Handles RoundedButton1.Click
         Dim openFileDialog As New OpenFileDialog()
-        OpenFileDialog1.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif"
+        openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp;*.gif"
 
         If openFileDialog.ShowDialog() = DialogResult.OK Then
             selectedImage = Image.FromFile(openFileDialog.FileName)
-            PictureBox1.Image = selectedImage
-            PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+            Try
+                PictureBox1.Image = selectedImage
+                PictureBox1.SizeMode = PictureBoxSizeMode.StretchImage
+            Catch
+                MessageBox.Show("Invalid Image")
+            End Try
+        End If
+    End Sub
 
+    Private Sub RoundedButton3_Click(sender As Object, e As EventArgs) Handles RoundedButton3.Click
+        Dim panelYPosition As Integer = Panel3.Controls.Count * 60
+
+        Dim panel1 As New Panel()
+        panel1.Size = New Size(242, 50)
+        panel1.Location = New Point(10, panelYPosition)
+        panel1.BackColor = Color.Transparent
+        AddHandler panel1.Paint, AddressOf Panel1_Paint
+
+        If String.IsNullOrWhiteSpace(RichTextBox3.Text) OrElse selectedImage Is Nothing Then
+            MessageBox.Show("Please select an image and provide a name before adding a panel.")
+            Return
         End If
 
+        AddHandler panel1.Click, AddressOf Panel_Click
+
+        Dim newPictureBox As New PictureBox()
+        newPictureBox.Size = New Size(85, 50)
+        newPictureBox.Image = selectedImage
+        newPictureBox.SizeMode = PictureBoxSizeMode.StretchImage
+        panel1.Controls.Add(newPictureBox)
+
+        AddHandler newPictureBox.Click, Sub(s, eArgs) Panel_Click(panel1, eArgs)
+
+        Dim newLabel As New Label()
+        newLabel.Text = RichTextBox3.Text
+        newLabel.Size = New Size(159, 44)
+        newLabel.Location = New Point(90, 3)
+        newLabel.BackColor = Color.White
+        newLabel.TextAlign = ContentAlignment.MiddleCenter
+        panel1.Controls.Add(newLabel)
+
+        AddHandler newLabel.Click, Sub(s, eArgs) Panel_Click(panel1, eArgs)
+
+        Panel3.Controls.Add(panel1)
+    End Sub
+
+    Private Sub Panel_Click(sender As Object, e As EventArgs)
+        selectedPanel = CType(sender, Panel)
+    End Sub
+
+    Private Sub RoundedButton4_Click(sender As Object, e As EventArgs) Handles RoundedButton4.Click
+        If selectedPanel IsNot Nothing Then
+            Dim result = MessageBox.Show($"Do you want to delete the selected panel with text '{selectedPanel.Controls.OfType(Of Label).FirstOrDefault()?.Text}'?", "Delete Panel", MessageBoxButtons.YesNo)
+            If result = DialogResult.Yes Then
+                Panel3.Controls.Remove(selectedPanel)
+                selectedPanel = Nothing
+
+                Dim currentYPosition As Integer = 0
+                For Each control As Control In Panel3.Controls
+                    If TypeOf control Is Panel Then
+                        Dim panel As Panel = CType(control, Panel)
+                        panel.Location = New Point(panel.Location.X, currentYPosition)
+                        currentYPosition += panel.Height + 10
+                    End If
+                Next
+            End If
+        Else
+            MessageBox.Show("No panel selected for deletion.")
+        End If
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs)
+        Dim panel As Panel = CType(sender, Panel)
+        Dim graphics As Graphics = e.Graphics
+        Dim rect As New Rectangle(0, 0, panel.Width, panel.Height)
+        Dim path As New Drawing2D.GraphicsPath()
+
+        Dim cornerRadius As Integer = 20
+        path.AddArc(rect.X, rect.Y, cornerRadius, cornerRadius, 180, 90)
+        path.AddArc(rect.Right - cornerRadius, rect.Y, cornerRadius, cornerRadius, 270, 90)
+        path.AddArc(rect.Right - cornerRadius, rect.Bottom - cornerRadius, cornerRadius, cornerRadius, 0, 90)
+        path.AddArc(rect.X, rect.Bottom - cornerRadius, cornerRadius, cornerRadius, 90, 90)
+        path.CloseFigure()
+
+        panel.Region = New Region(path)
+    End Sub
+
+    Private Sub RoundedButton5_Click(sender As Object, e As EventArgs) Handles RoundedButton5.Click
+        selectedImage = Nothing
+        RichTextBox3.Text = ""
+        RichTextBox2.Text = ""
+        MessageBox.Show("Restarted selected items successfully!")
     End Sub
 End Class
