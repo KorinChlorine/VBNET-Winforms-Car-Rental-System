dd5da3ea9bedbf122f893d8be1cb264d96e90acf
ChlorineKorin <kharlasuncion1010@gmail.com>
2025-04-29 03:48:36 +0800

    added homepage

---
+++ homeForm.vb
@@ -0,0 +1,11 @@
+﻿Public Class homeForm
+    Private Sub homeForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Me.Hide()
+        rent_a_car.Show()
+    End Sub
+
+End Class
