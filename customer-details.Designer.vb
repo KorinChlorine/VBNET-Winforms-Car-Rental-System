b9fb6604f035311c907aa5e1f9ed1995d9d7230d
ChlorineKorin <kharlasuncion1010@gmail.com>
2025-04-28 11:07:28 +0800

    #

--- customer-details.Designer.vb
+++ customer-details.Designer.vb
@@ -274,7 +274,7 @@ Partial Class Form1
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Text = "A"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
