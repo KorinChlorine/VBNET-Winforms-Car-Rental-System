322f871edb477f8beb88ce70835540673a09986a
ChlorineKorin <kharlasuncion1010@gmail.com>
2025-04-29 03:14:50 +0800

    Added online and offline database

--- LoginForm.vb
+++ LoginForm.vb
@@ -4,17 +4,11 @@ Imports System.Windows.Input
 Imports MySql.Data.MySqlClient
 Imports System.Data
 
-
-
-'notes:
-'emailLogin = textbox1.text
-'passLogin = textbox2.text
-'emailRegister = textbox3.text
-'passRegister = textbox4.text
-
-
-
 Public Class LoginForm
+    Dim conn As MySqlConnection
+    Dim COMMAND As MySqlCommand
+    Dim users As New List(Of Tuple(Of String, String)) ' Email, Password for fallback
+
     Private Sub LoginForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         AxWindowsMediaPlayer1.URL = Path.Combine(Application.StartupPath, "video.mp4")
         AxWindowsMediaPlayer1.settings.setMode("loop", True)
@@ -29,14 +23,19 @@ Public Class LoginForm
         Button4.Visible = False
         CheckBox1.Visible = True
 
-    End Sub
+        conn = New MySqlConnection
+        conn.ConnectionString = "server=127.0.0.1;userid=root;password='';database=test"
 
-    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
-
+        Try
+            conn.Open()
+            MessageBox.Show("Connection to MySQL Successful")
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            conn.Close()
+        End Try
     End Sub
 
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-
         Panel1.BackgroundImage = My.Resources.Register1
         TextBox1.Visible = False
         TextBox2.Visible = False
@@ -44,42 +43,89 @@ Public Class LoginForm
         TextBox4.Visible = True
         TextBox5.Visible = True
         CheckBox1.Visible = False
+        Button4.Visible = True
         Button3.Visible = False
         Button5.Visible = True
-
     End Sub
 
-
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        'login
+        ' Login
+        Dim emailLogin As String = TextBox1.Text
+        Dim passLogin As String = TextBox2.Text
 
-        'userTrial
-        'If (textbox1.text = "user") and (textbox2.text = "user" then
-        'AxWindowsMediaPlayer1.Ctlcontrols.stop()
-        'Me.hide()
-        'homePage.show()
+        Try
+            ' Existing MySQL login code
+            Dim connectionString As String = "server=127.0.0.1;userid=root;password='';database=user information"
+            Dim query As String = "SELECT email FROM `user info` WHERE email = @Email AND password = @Password"
 
-        'admin
-        'If (textbox1.text = "admin") and (textbox2.text = "admin" then
-        'AxWindowsMediaPlayer1.Ctlcontrols.stop()
-        'Me.hide()
-        'management.show()
+            Using con As New MySqlConnection(connectionString)
+                Using cmd As New MySqlCommand(query, con)
+                    cmd.Parameters.AddWithValue("@Email", emailLogin)
+                    cmd.Parameters.AddWithValue("@Password", passLogin)
 
+                    con.Open()
+                    Dim result As Object = cmd.ExecuteScalar()
+
+                    If result IsNot Nothing Then
+                        AxWindowsMediaPlayer1.Ctlcontrols.stop()
+                        Me.Hide()
+                        'homePage.Show()
+                    Else
+                        MessageBox.Show("Wrong Email/Password, Please Try Again!")
+                    End If
+
+                    con.Close()
+                End Using
+            End Using
+        Catch ex As Exception
+            ' Fallback to array login
+            Dim user = users.FirstOrDefault(Function(u) u.Item1 = emailLogin AndAlso u.Item2 = passLogin)
+            If user IsNot Nothing Then
+                AxWindowsMediaPlayer1.Ctlcontrols.stop()
+                Me.Hide()
+                'homePage.Show()
+            Else
+                MessageBox.Show("Wrong Email/Password, Please Try Again!")
+            End If
+        End Try
     End Sub
 
     Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
-        'register
-
-        'email
+        ' Register
         Dim emailRegister As String = TextBox3.Text
+        Dim passRegister As String = TextBox4.Text
+        Dim confirmPass As String = TextBox5.Text
 
-        'pass + confirm
-        If TextBox4.Text = TextBox5.Text Then
-            Dim passRegister As String = TextBox4.Text
+        If passRegister = confirmPass Then
+            Try
+                ' Existing MySQL registration code
+                Dim connectionString As String = "server=127.0.0.1;userid=root;password='';database=user information"
+                Dim query As String = "INSERT INTO `user info` (email, password) VALUES (@Email, @Password)"
+
+                Using con As New MySqlConnection(connectionString)
+                    Using cmd As New MySqlCommand(query, con)
+                        cmd.Parameters.AddWithValue("@Email", emailRegister)
+                        cmd.Parameters.AddWithValue("@Password", passRegister)
+
+                        con.Open()
+                        cmd.ExecuteNonQuery()
+                        con.Close()
+
+                        MessageBox.Show("Registration successful!")
+                    End Using
+                End Using
+            Catch ex As Exception
+                ' Fallback to array registration
+                If users.Any(Function(u) u.Item1 = emailRegister) Then
+                    MessageBox.Show("User already exists!")
+                Else
+                    users.Add(Tuple.Create(emailRegister, passRegister))
+                    MessageBox.Show("Registration successful!")
+                End If
+            End Try
+        Else
+            MessageBox.Show("Passwords do not match!")
         End If
-
-
-
     End Sub
 
     Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
@@ -90,9 +136,8 @@ Public Class LoginForm
         TextBox1.Visible = True
         TextBox2.Visible = True
         TextBox5.Visible = False
-        Button4.Visible = True
+        Button3.Visible = True
+        Button4.Visible = False
         CheckBox1.Visible = True
-        TextBox5.Visible = False
-
     End Sub
 End Class
