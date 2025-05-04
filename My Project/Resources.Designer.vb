711f28bb068053a9f8bc02e041864ce65071d0b3
RenBergun <bergundojarren17@gmail.com>
2025-05-04 14:33:13 +0800

    added temporary buttons, may button na side panel and stuff

--- My Project/Resources.Designer.vb
+++ My Project/Resources.Designer.vb
@@ -73,6 +73,16 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property Main_Menu_3() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("Main Menu 3", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Looks up a localized resource of type System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property Main_Menu1() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("Main Menu1", resourceCulture)
--- homepage.Designer.vb
+++ homepage.Designer.vb
@@ -22,19 +22,227 @@ Partial Class homepage
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.btnUndeclared = New System.Windows.Forms.Button()
+        Me.btnRentalDetails = New System.Windows.Forms.Button()
+        Me.btnViewCar = New System.Windows.Forms.Button()
+        Me.btnBilling = New System.Windows.Forms.Button()
+        Me.btnSettings = New System.Windows.Forms.Button()
+        Me.btnExit = New System.Windows.Forms.Button()
+        Me.btn2RentalDetails = New System.Windows.Forms.Button()
+        Me.btn2ViewCar = New System.Windows.Forms.Button()
+        Me.btnHistory = New System.Windows.Forms.Button()
+        Me.btn2Billing = New System.Windows.Forms.Button()
+        Me.btnProfile = New System.Windows.Forms.Button()
+        Me.btnHome = New System.Windows.Forms.Button()
+        Me.btnRentACar = New System.Windows.Forms.Button()
+        Me.txtboxSearch = New System.Windows.Forms.TextBox()
         Me.SuspendLayout()
+        '
+        'btnUndeclared
+        '
+        Me.btnUndeclared.BackColor = System.Drawing.Color.Transparent
+        Me.btnUndeclared.FlatAppearance.BorderSize = 0
+        Me.btnUndeclared.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+        Me.btnUndeclared.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.btnUndeclared.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnUndeclared.Location = New System.Drawing.Point(24, 187)
+        Me.btnUndeclared.Name = "btnUndeclared"
+        Me.btnUndeclared.Size = New System.Drawing.Size(52, 27)
+        Me.btnUndeclared.TabIndex = 2
+        Me.btnUndeclared.UseVisualStyleBackColor = False
+        '
+        'btnRentalDetails
+        '
+        Me.btnRentalDetails.BackColor = System.Drawing.Color.Transparent
+        Me.btnRentalDetails.FlatAppearance.BorderSize = 0
+        Me.btnRentalDetails.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent
+        Me.btnRentalDetails.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+        Me.btnRentalDetails.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.btnRentalDetails.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnRentalDetails.Location = New System.Drawing.Point(24, 245)
+        Me.btnRentalDetails.Name = "btnRentalDetails"
+        Me.btnRentalDetails.Size = New System.Drawing.Size(52, 32)
+        Me.btnRentalDetails.TabIndex = 3
+        Me.btnRentalDetails.UseVisualStyleBackColor = False
+        '
+        'btnViewCar
+        '
+        Me.btnViewCar.BackColor = System.Drawing.Color.Transparent
+        Me.btnViewCar.FlatAppearance.BorderSize = 0
+        Me.btnViewCar.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent
+        Me.btnViewCar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+        Me.btnViewCar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.btnViewCar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnViewCar.Location = New System.Drawing.Point(24, 313)
+        Me.btnViewCar.Name = "btnViewCar"
+        Me.btnViewCar.Size = New System.Drawing.Size(52, 27)
+        Me.btnViewCar.TabIndex = 4
+        Me.btnViewCar.UseVisualStyleBackColor = False
+        '
+        'btnBilling
+        '
+        Me.btnBilling.BackColor = System.Drawing.Color.Transparent
+        Me.btnBilling.FlatAppearance.BorderSize = 0
+        Me.btnBilling.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent
+        Me.btnBilling.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+        Me.btnBilling.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.btnBilling.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnBilling.Location = New System.Drawing.Point(24, 370)
+        Me.btnBilling.Name = "btnBilling"
+        Me.btnBilling.Size = New System.Drawing.Size(52, 36)
+        Me.btnBilling.TabIndex = 5
+        Me.btnBilling.UseVisualStyleBackColor = False
+        '
+        'btnSettings
+        '
+        Me.btnSettings.BackColor = System.Drawing.Color.Transparent
+        Me.btnSettings.FlatAppearance.BorderSize = 0
+        Me.btnSettings.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent
+        Me.btnSettings.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+        Me.btnSettings.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.btnSettings.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnSettings.Location = New System.Drawing.Point(24, 538)
+        Me.btnSettings.Name = "btnSettings"
+        Me.btnSettings.Size = New System.Drawing.Size(52, 35)
+        Me.btnSettings.TabIndex = 6
+        Me.btnSettings.UseVisualStyleBackColor = False
+        '
+        'btnExit
+        '
+        Me.btnExit.BackColor = System.Drawing.Color.Transparent
+        Me.btnExit.FlatAppearance.BorderSize = 0
+        Me.btnExit.FlatAppearance.CheckedBackColor = System.Drawing.Color.Transparent
+        Me.btnExit.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+        Me.btnExit.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.btnExit.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnExit.Location = New System.Drawing.Point(24, 595)
+        Me.btnExit.Name = "btnExit"
+        Me.btnExit.Size = New System.Drawing.Size(52, 28)
+        Me.btnExit.TabIndex = 7
+        Me.btnExit.UseVisualStyleBackColor = False
+        '
+        'btn2RentalDetails
+        '
+        Me.btn2RentalDetails.Location = New System.Drawing.Point(182, 370)
+        Me.btn2RentalDetails.Name = "btn2RentalDetails"
+        Me.btn2RentalDetails.Size = New System.Drawing.Size(75, 23)
+        Me.btn2RentalDetails.TabIndex = 8
+        Me.btn2RentalDetails.Text = "Button9"
+        Me.btn2RentalDetails.UseVisualStyleBackColor = True
+        '
+        'btn2ViewCar
+        '
+        Me.btn2ViewCar.Location = New System.Drawing.Point(208, 455)
+        Me.btn2ViewCar.Name = "btn2ViewCar"
+        Me.btn2ViewCar.Size = New System.Drawing.Size(75, 23)
+        Me.btn2ViewCar.TabIndex = 9
+        Me.btn2ViewCar.Text = "Button10"
+        Me.btn2ViewCar.UseVisualStyleBackColor = True
+        '
+        'btnHistory
+        '
+        Me.btnHistory.Location = New System.Drawing.Point(516, 370)
+        Me.btnHistory.Name = "btnHistory"
+        Me.btnHistory.Size = New System.Drawing.Size(75, 23)
+        Me.btnHistory.TabIndex = 10
+        Me.btnHistory.Text = "Button11"
+        Me.btnHistory.UseVisualStyleBackColor = True
+        '
+        'btn2Billing
+        '
+        Me.btn2Billing.Location = New System.Drawing.Point(516, 455)
+        Me.btn2Billing.Name = "btn2Billing"
+        Me.btn2Billing.Size = New System.Drawing.Size(75, 23)
+        Me.btn2Billing.TabIndex = 11
+        Me.btn2Billing.Text = "Button12"
+        Me.btn2Billing.UseVisualStyleBackColor = True
+        '
+        'btnProfile
+        '
+        Me.btnProfile.Location = New System.Drawing.Point(1029, 22)
+        Me.btnProfile.Name = "btnProfile"
+        Me.btnProfile.Size = New System.Drawing.Size(75, 23)
+        Me.btnProfile.TabIndex = 12
+        Me.btnProfile.Text = "Button13"
+        Me.btnProfile.UseVisualStyleBackColor = True
+        '
+        'btnHome
+        '
+        Me.btnHome.BackColor = System.Drawing.Color.Transparent
+        Me.btnHome.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None
+        Me.btnHome.FlatAppearance.BorderSize = 0
+        Me.btnHome.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+        Me.btnHome.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnHome.Location = New System.Drawing.Point(12, 65)
+        Me.btnHome.Name = "btnHome"
+        Me.btnHome.Size = New System.Drawing.Size(75, 38)
+        Me.btnHome.TabIndex = 13
+        Me.btnHome.UseVisualStyleBackColor = False
+        '
+        'btnRentACar
+        '
+        Me.btnRentACar.BackColor = System.Drawing.Color.Transparent
+        Me.btnRentACar.FlatAppearance.BorderSize = 0
+        Me.btnRentACar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Transparent
+        Me.btnRentACar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Transparent
+        Me.btnRentACar.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.btnRentACar.Location = New System.Drawing.Point(24, 125)
+        Me.btnRentACar.Name = "btnRentACar"
+        Me.btnRentACar.Size = New System.Drawing.Size(52, 34)
+        Me.btnRentACar.TabIndex = 14
+        Me.btnRentACar.UseVisualStyleBackColor = False
+        '
+        'txtboxSearch
+        '
+        Me.txtboxSearch.BackColor = System.Drawing.Color.MediumSlateBlue
+        Me.txtboxSearch.BorderStyle = System.Windows.Forms.BorderStyle.None
+        Me.txtboxSearch.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.txtboxSearch.Location = New System.Drawing.Point(766, 55)
+        Me.txtboxSearch.Name = "txtboxSearch"
+        Me.txtboxSearch.Size = New System.Drawing.Size(213, 19)
+        Me.txtboxSearch.TabIndex = 15
         '
         'homepage
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.BackgroundImage = Global.VBNET_Car_Rental_System.My.Resources.Resources.Main_Menu1
+        Me.BackgroundImage = Global.VBNET_Car_Rental_System.My.Resources.Resources.Main_Menu_3
         Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
         Me.ClientSize = New System.Drawing.Size(1264, 681)
+        Me.Controls.Add(Me.txtboxSearch)
+        Me.Controls.Add(Me.btnRentACar)
+        Me.Controls.Add(Me.btnHome)
+        Me.Controls.Add(Me.btnProfile)
+        Me.Controls.Add(Me.btn2Billing)
+        Me.Controls.Add(Me.btnHistory)
+        Me.Controls.Add(Me.btn2ViewCar)
+        Me.Controls.Add(Me.btn2RentalDetails)
+        Me.Controls.Add(Me.btnExit)
+        Me.Controls.Add(Me.btnSettings)
+        Me.Controls.Add(Me.btnBilling)
+        Me.Controls.Add(Me.btnViewCar)
+        Me.Controls.Add(Me.btnRentalDetails)
+        Me.Controls.Add(Me.btnUndeclared)
         Me.DoubleBuffered = True
         Me.Name = "homepage"
         Me.Text = "homepage"
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents btnUndeclared As Button
+    Friend WithEvents btnRentalDetails As Button
+    Friend WithEvents btnViewCar As Button
+    Friend WithEvents btnBilling As Button
+    Friend WithEvents btnSettings As Button
+    Friend WithEvents btnExit As Button
+    Friend WithEvents btn2RentalDetails As Button
+    Friend WithEvents btn2ViewCar As Button
+    Friend WithEvents btnHistory As Button
+    Friend WithEvents btn2Billing As Button
+    Friend WithEvents btnProfile As Button
+    Friend WithEvents btnHome As Button
+    Friend WithEvents btnRentACar As Button
+    Friend WithEvents txtboxSearch As TextBox
 End Class
